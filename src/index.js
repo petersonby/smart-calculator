d0e8949034c3e210df8df86a116ab586aa39204f
Peterson <contact@peterson.by>
2018-10-03 00:45:11 +0300

    implementing smart calculator class

--- src/index.js
+++ src/index.js
@@ -1,27 +1,99 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.stack = [];
+    this.stack.push(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.stack.push('+', number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.stack.push('-', number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.stack.push('*', number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.stack.push('/', number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.stack.push('^', number);
+    return this;
+  }
+
+  valueOf() {
+    const operators = {
+      '+': {
+        func: (x, y) => x + y,
+        priority: 1
+      },
+      '-': {
+        func: (x, y) => x - y,
+        priority: 1
+      },
+      '*': {
+        func: (x, y) => x * y,
+        priority: 2
+      },
+      '/': {
+        func: (x, y) => x / y,
+        priority: 2
+      },
+      '^': {
+        func: (x, y) => Math.pow(x, y),
+        priority: 3
+      }
+    };
+
+    let polish = (infix) => {
+      const otherStack = [];
+      const peek = (a) => a[a.length - 1];
+
+      return infix
+        .reduce((output, token) => {
+          if (parseFloat(token)) {
+            output.push(token);
+          }
+
+          if (token in operators) {
+            let right = token === '^';
+            while (peek(otherStack) in operators && (
+              (operators[token].priority <= operators[peek(otherStack)].priority && !right) ||
+              (operators[token].priority < operators[peek(otherStack)].priority && right)
+            ))
+              output.push(otherStack.pop());
+            otherStack.push(token);
+          }
+          
+          return output;
+        }, [])
+        .concat(otherStack.reverse());
+    }    
+
+    let value = (expr) => {
+      const otherStack = [];
+
+      expr.forEach((token) => {
+        if (token in operators) {
+          let [y, x] = [otherStack.pop(), otherStack.pop()];
+          otherStack.push(operators[token].func(x, y));
+        } else {
+          otherStack.push(token);
+        }    
+      });
+
+      return otherStack.pop();
+    }
+
+    return value(polish(this.stack));
   }
 }
-
 module.exports = SmartCalculator;
